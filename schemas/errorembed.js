c9b0655c1b2628041597236dea9f6901adcd2cad
Zeroknights <73501749+Zeroknights16@users.noreply.github.com>
2021-07-07 20:56:27 +0200

    Update errorembed.js

--- schemas/errorembed.js
+++ schemas/errorembed.js
@@ -23,17 +23,6 @@ module.exports = function errorembed(message, errortype, commandName, caseid) {
 		console.log(`${timess} | ` + consolewords('[CMD]: ') + consoleerrcmds(`${message.author.username}: Missing Arguments => Error => ID: ${command.id}, Case ID: ${caseid}`));
 		return;
 	}
-	else if (errortype === 'devmode') {
-		const errorPermEmbed = new Discord.MessageEmbed()
-			.setColor('#FE9A2E')
-			.setTitle(`⛔ Error (${config['bot'].prefix}${command.name}) `)
-			.setDescription(`<@${message.author.id}>, ${config['messages'].devmode}\n\n> **ID:** \`\`${command.id}\`\`\n> **Module:** \`\`${command.activated}\`\`\n> **Case ID:** \`\`${caseid}\`\`\n\n_If you think this is an error report it to a staff member via tickets_`)
-			.setTimestamp()
-			.setFooter(`${config['server'].name} Nuker Bot`);
-		message.channel.send({ embed: errorPermEmbed });
-		console.log(`${timess} | ` + consolewords('[CMD]: ') + consoleerrcmds(`${message.author.username}: Dev Mode enabled => Error => ID: ${command.id}, Case ID: ${caseid}`));
-		return;
-	}
 	else if (errortype === 'guildonly') {
 		const errorPermEmbed = new Discord.MessageEmbed()
 			.setColor('#FE9A2E')
@@ -43,39 +32,6 @@ module.exports = function errorembed(message, errortype, commandName, caseid) {
 			.setFooter(`${config['server'].name} Nuker Bot`);
 		message.channel.send({ embed: errorPermEmbed });
 		console.log(`${timess} | ` + consolewords('[CMD]: ') + consoleerrcmds(`${message.author.username}: Dms => Error => ID: ${command.id}, Case ID: ${caseid}`));
-		return;
-	}
-	else if (errortype === 'activated') {
-		const errorPermEmbed = new Discord.MessageEmbed()
-			.setColor('#FE9A2E')
-			.setTitle(`⛔ Error (${config['bot'].prefix}${command.name}) `)
-			.setDescription(`<@${message.author.id}>, ${config['messages'].enabled}\n\n> **ID:** \`\`${command.id}\`\`\n> **Module:** \`\`${command.activated}\`\`\n> **Case ID:** \`\`${caseid}\`\`\n\n_If you think this is an error report it to a staff member via tickets_`)
-			.setTimestamp()
-			.setFooter(`${config['server'].name} Nuker Bot`);
-		message.channel.send({ embed: errorPermEmbed });
-		console.log(`${timess} | ` + consolewords('[CMD]: ') + consoleerrcmds(`${message.author.username}: Module Disabled => Error => ID: ${command.id}, Case ID: ${caseid}`));
-		return;
-	}
-	else if (errortype === 'usermention') {
-		const errorArgsEmbed = new Discord.MessageEmbed()
-			.setColor('#FE9A2E')
-			.setTitle(`⛔ Error (${config['bot'].prefix}${command.name}) `)
-			.setDescription(`<@${message.author.id}>, ${config['messages'].usermention}\n\n> **ID:** \`\`${command.id}\`\`\n> **Usage:** \`\`${config['bot'].prefix}${command.usage}\`\`\n> **Case ID:** \`\`${caseid}\`\`\n\n_If you think this is an error report it to a staff member via tickets_`)
-			.setTimestamp()
-			.setFooter(`${config['messages'].errfooter}`);
-		message.channel.send({ embed: errorArgsEmbed });
-		console.log(`${timess} | ` + consolewords('[CMD]: ') + consoleerrcmds(`${message.author.username}: Missing Arguments => Error => ID: ${command.id}, Case ID: ${caseid}`));
-		return;
-	}
-	else if (errortype === 'rolemention') {
-		const errorRoleEmbed = new Discord.MessageEmbed()
-			.setColor('#FE9A2E')
-			.setTitle(`⛔ Error (${config['bot'].prefix}${command.name}) `)
-			.setDescription(`<@${message.author.id}>, ${config['messages'].rolemention}\n\n> **ID:** \`\`${command.id}\`\`\n> **Usage:** \`\`${config['bot'].prefix}${command.usage}\`\`\n> **Case ID:** \`\`${caseid}\`\`\n\n_If you think this is an error report it to a staff member via tickets_`)
-			.setTimestamp()
-			.setFooter(`${config['messages'].errfooter}`);
-		message.channel.send({ embed: errorRoleEmbed });
-		console.log(`${timess} | ` + consolewords('[CMD]: ') + consoleerrcmds(`${message.author.username}: Missing Mention => Error => ID: ${command.id}, Case ID: ${caseid}`));
 		return;
 	}
 	else if (errortype === 'permission') {
